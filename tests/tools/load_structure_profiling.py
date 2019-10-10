cf5203c66f9b29f7da1fe37fc79208c3a9a102aa
vdhert <tymoteusz.oleniecki@gmail.com>
2019-10-10 14:57:45 +0200

    Add test tool for structure loading profiling

---
+++ tests/tools/load_structure_profiling.py
@@ -0,0 +1,18 @@
+import cProfile
+import datetime
+import os.path
+
+from pydesc.structure import StructureLoader
+from tests.conftest import TEST_STRUCTURES_DIR
+
+
+def run():
+    sl = StructureLoader()
+    pth = os.path.join(TEST_STRUCTURES_DIR, 'prots_only_nmr', '1A24.pdb')
+    s = sl.load_structures(path=pth)
+    return s
+
+
+if __name__ == '__main__':
+    now = datetime.datetime.now().strftime('%d-%m_%H-%M-%S')
+    cProfile.run('run()', 'profile_%s' % now)
