4700d4f01651ac9f17c3fe783584d05319f1e420
vdhert <tymoteusz.oleniecki@gmail.com>
2020-07-17 17:49:05 +0200

    Add docstrings, remove spare parts

--- pydesc/contacts/base.py
+++ pydesc/contacts/base.py
@@ -15,12 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with PyDesc.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-Base class for contact criteria, combined criteria and utility functions
-that deal helps in dealing with contacts among mers present in PyDesc
-(sub)structures.
+"""Base and auxiliary classes for contact criteria.
 
-created: 20.05.2019 - , Tymoteusz 'hert' Oleniecki
+Contact criteria are used to calculate contact maps for structures.
+Pre-defined, ready to use criteria are stored in `criteria.py` submodule.
+
 """
 
 import re
@@ -34,27 +33,82 @@ from pydesc.selection import Everything
 
 
 class ContactCriterion(metaclass=ABCMeta):
-    """Abstract class, criteria instances."""
+    """Abstract criterion, base for all other criteria."""
 
     def __init__(self):
         self.selection1 = Everything()
         self.selection2 = Everything()
 
     def set_selections(self, selection1, selection2):
+        """Set selections determining for which mers contacts will be calculated.
+        
+        E.g. for criteria that only make sense for residues, but does not for
+        nucleotides, one would like to add selections picking residues only.
+        
+        Note that this allows to define contacts between different types of mers,
+        e.g. between nucleotides and residues.
+
+        Args:
+            selection1: selection instance. By default Everything selection is set.
+            selection2: selection instance. By default Everything selection is set.
+
+        Returns:
+
+        """
         self.selection1 = selection1
         self.selection2 = selection2
 
     def calculate_contacts(self, structure_obj):
+        """Calculate all contacts in given structure.
+        
+        Contacts are usually sparse, so this method by default returns scipy dok
+        matrix. Note that shape of returned matrix takes into account all mers from
+        structure given structure was derived from. Size of returned matrix depends
+        on number of inds recognised in structure's converter.
+        
+        In subclasses in most cases it should be sufficient to leave that method
+        untouched and overwrite _fill_contact_matrix instead.
+
+        Args:
+            structure_obj: any pydesc
+
+        Returns:
+            scipy.sparse.dok_matrix: square matrix filled with contact values. Each
+            row and column corresponds with mer ind, so for substructures returned
+            matrix will have probably greater dimension than expected.
+
+        """
         mers1 = self.selection1.create_structure(structure_obj)
         mers2 = self.selection2.create_structure(structure_obj)
 
-        total_len = structure_obj.converter.get_max_ind()
+        total_len = structure_obj.derived_from.converter.get_max_ind()
         contact_map = dok_matrix((total_len, total_len), dtype=numpy.uint8)
         contact_map = self._fill_contact_matrix(mers1, mers2, contact_map)
 
         return contact_map
 
     def _fill_contact_matrix(self, mers1, mers2, matrix):
+        """Fill given contact matrix for given two sets of mers.
+        
+        Meant to be overwritten in subclasses. ContactCriterion gives rather
+        suboptimal, generic implementation, which requires implementation of
+        _calculate_contact method. So there are two ways of writing subclasses:
+        either overwrite _fill_contact_matrix, or leave it as is and implement
+        _calculate_contact.
+        
+        This method suppose to fill given matrix with values so that element i,
+        j store value of contact between i-th mer from mers1 and j-th mer from mers2,
+        where i and j are corresponding mers inds (values of ind attribute).
+
+        Args:
+            mers1: sequence of mers.
+            mers2: other sequence of mers (possibly the same).
+            matrix: matrix of appropriate shape to be filled with calculated values.
+
+        Returns:
+            given matrix, filled with calculated values.
+
+        """
         for mer1 in mers1:
             for mer2 in mers2:
                 value = self._calculate_contact(mer1, mer2)
@@ -62,6 +116,21 @@ class ContactCriterion(metaclass=ABCMeta):
         return matrix
 
     def _calculate_contact(self, mer1, mer2):
+        """Calculate value of single contact between given mers.
+        
+        Meant to be overwritten in subclasses that do not have optimal vectorised
+        implementation (in which case they overwrite _fill_contact_matrix method).
+        For all others -- raise NotImplemented error.
+
+        Args:
+            mer1: first mer instance.
+            mer2: second mer instance.
+
+        Returns:
+            int: contact value (0 if there is no contact, 1 for uncertain contact
+            or 2 for sure one).
+
+        """
         raise NotImplemented
 
     def __or__(self, other):
@@ -83,6 +152,13 @@ class ContactCriterion(metaclass=ABCMeta):
 
 
 class NotCriterion(ContactCriterion):
+    """Class reverting criterion calculations.
+
+    Args:
+        criterion: contact criterion instance.
+
+    """
+
     def __str__(self):
         criterion_repr = str(self.criterion)
         return f"not ({criterion_repr})"
@@ -93,21 +169,35 @@ class NotCriterion(ContactCriterion):
         self.set_selections(criterion.selection1, criterion.selection2)
 
     def calculate_contacts(self, structure_obj):
+        """Calculate contacts and return value matrix.
+
+        Overwrites superclass method.
+        Calculates contacts using criterion to negate and returns negative (in
+        three-value logic) matrix.
+
+        Args:
+            structure_obj: structure instance.
+
+        Returns:
+            scipy.sparse.dok_matrix: matrix of contact values in given structure.
+
+        """
         contact_map = self.criterion.calculate_contacts(structure_obj)
         new = (contact_map.todense().astype(numpy.int8) - 1) * -1 + 1
         return dok_matrix(new)
+        # TODO: there should be better way than changing to dense
 
 
 class CombinedContact(ContactCriterion):
     """Abstract class, criteria obtained via logical operations on contact
-    criteria."""
+    criteria.
+
+    Args:
+        any number of other criteria.
+
+    """
 
     def __init__(self, *criteria_objs):
-        """Combined criteria constructor.
-
-        Arguments:
-        criteria_objs -- basic criteria objects.
-        """
         n_criteria = len(criteria_objs)
         if n_criteria < 2:
             raise AttributeError("Need at least two criteria to combine.")
@@ -116,8 +206,7 @@ class CombinedContact(ContactCriterion):
         super().__init__()
 
     def _repr_operation(self):
-        """Returns regular expression operation to be used in __repr__ and
-        __str__."""
+        """Returns regular expression operation to be used in __repr__ and __str__."""
         pattern = re.compile("[A-Z][a-z]*")
         name = self.__class__.__name__.replace("Contacts", "")
         operation = " ".join(re.findall(pattern, name)).lower()
@@ -135,6 +224,7 @@ class CombinedContact(ContactCriterion):
 
 
 class ContactsConjunction(CombinedContact):
+    """Conjunction of contact criteria."""
 
     def __init__(self, *criteria_objs):
         super().__init__(*criteria_objs)
@@ -147,6 +237,19 @@ class ContactsConjunction(CombinedContact):
         self.selection2 = selection2
 
     def calculate_contacts(self, structure_obj):
+        """Calculate conjunction of stored criteria.
+
+        Value of 2 meant that all criteria were surely satisfied, while value of 0
+        means that at least one criterion was not satisfied. 1 indicates that at
+        least one criterion was uncertainly satisfied, while rest was surely satisfied.
+
+        Args:
+            structure_obj: structure instance.
+
+        Returns:
+            scipy.sparse: matrix of contact values for given structure.
+
+        """
         contact_map = self.criteria[0].calculate_contacts(structure_obj)
         for criterion in self.criteria[1:]:
             new_map = criterion.calculate_contacts(structure_obj)
@@ -155,8 +258,22 @@ class ContactsConjunction(CombinedContact):
 
 
 class ContactsAlternative(CombinedContact):
+    """Alternative of contact criteria."""
 
     def calculate_contacts(self, structure_obj):
+        """Calculate alternative of stored criteria.
+
+        Value of 2 means that at least one of given criteria was surely satisfied,
+        while 0 means that none of them was. 1 indicates that at least one criterion
+        was uncertainly satisfied, while rest was not satisfied.
+
+        Args:
+            structure_obj: structure instance.
+
+        Returns:
+            scipy.sparse: matrix of contact values for given structure.
+
+        """
         contact_map = self.criteria[0].calculate_contacts(structure_obj)
         for criterion in self.criteria[1:]:
             new_map = criterion.calculate_contacts(structure_obj)
@@ -165,8 +282,20 @@ class ContactsAlternative(CombinedContact):
 
 
 class ContactsExclusiveDisjunction(CombinedContact):
+    """Exclusive disjunction (xor) of contact criteria."""
 
     def calculate_contacts(self, structure_obj):
+        """Calculate xor of stored criteria.
+
+        2 means that exactly one criterion was satisfied.
+
+        Args:
+            structure_obj: structure instance.
+
+        Returns:
+            scipy.sparse: matrix of contact values for given structure.
+
+        """
         new_map = self.criteria[0].calculate_contacts(structure_obj)
         count_ones = (new_map == 1).astype(numpy.uint8)
         count_twos = (new_map == 2).astype(numpy.uint8)
--- pydesc/contacts/geometrical.py
+++ pydesc/contacts/geometrical.py
@@ -1,3 +1,4 @@
+"""Contact criteria based on geometrical features of mers."""
 import numpy
 from scipy.spatial.distance import cdist
 
@@ -5,10 +6,35 @@ from pydesc.contacts.base import ContactCriterion
 
 
 def get_mer_inds(mers):
+    """Get list of ind of given mers.
+
+    Args:
+        mers: sequence of mers.
+
+    Returns:
+        numpy.array: of mers inds in the same order as mers in given sequence.
+
+    """
     return numpy.array([mer.ind for mer in mers])
 
 
 def get_distance_matrix(mers1, mers2, point):
+    """Calculate distances between given (pseudo)atoms of two sets of mers.
+
+    If any mer from any of given sequence does not have attribute *point*,
+    this function will raise AttributeError.
+
+    Args:
+        mers1: sequence of mer instances.
+        mers2: second sequence of mer instances (can be the same as mers1).
+        point(str): name of point to measure distance from (e.g. 'rc'), atom or
+        pseudoatom.
+
+    Returns:
+        : matrix of shape (len(mers1), len(mers2)) storing distances between
+        appropriate (pseudo)atoms.
+
+    """
     points1 = numpy.array([getattr(mer, point).vector for mer in mers1])
     points2 = numpy.array([getattr(mer, point).vector for mer in mers2])
     dist_mtx = cdist(points1, points2)
@@ -16,6 +42,20 @@ def get_distance_matrix(mers1, mers2, point):
 
 
 class PointsDistanceCriterion(ContactCriterion):
+    """Criterion based on distance between atoms or pseudoatoms.
+
+    This criterion measures distance between two atoms or pseudoatoms of two mers (
+    both mers have to have such (pseudo)atom) than compares it to threshold.
+    Everything below threshold - margin counts as sure contact (2) while distances
+    between threshold - margin and threshold + margin are considered uncertain (1).
+
+    Args:
+        atom_name(str): name of (pseudo)atom.
+        threshold(float): distance threshold.
+        margin(float): uncertain range from threshold.
+
+    """
+
     def __str__(self):
         return f"distance between {self.atom_name}"
 
@@ -48,6 +88,24 @@ class PointsDistanceCriterion(ContactCriterion):
 
 
 class DistancesDifferenceCriterion(ContactCriterion):
+    """Criterion based on difference between distances of two different points.
+
+    For each pair it calculates distances between two (pseudo)atoms of the same kind,
+    e.g. 'ca' and 'cbx' for residues. Then it subtracts those distances and compares
+    difference with threshold (taking uncertain margin into account). That implements
+    more abstract idea of checking if two vectors present in mer (in example above:
+    ca->cbx) are pointing more or less at each other.
+    Note that during comparison actual thresholds for values 2 and 1 are calculated
+    as, respectively, threshold + margin and threshold - margin.
+
+    Args:
+        point1(str): name of 1st (pseudo)atom.
+        point2(str): name of 2nd (pseudo)atom.
+        threshold(float): distance difference threshold.
+        margin(float): distance difference uncertain margin.
+
+    """
+
     def __str__(self):
         return f"distance difference between {self.point1} and {self.point2}"
 
