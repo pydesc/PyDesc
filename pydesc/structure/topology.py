49e96437736aa6475218bad6702efe2c2c8f5d69
vdhert <tymoteusz.oleniecki@gmail.com>
2020-07-29 00:01:46 +0200

    Fix previous_mer attr leftovers

--- pydesc/structure/topology.py
+++ pydesc/structure/topology.py
@@ -19,12 +19,12 @@ class BackbonedMixIn:
     def _fill_mers_attrs(self):
         """Sets mers attributes normally set by init.
 
-        Sets next/previous_mer attributes.
+        Sets next/prev_mer attributes.
         """
         for pair in zip(self._mers[:-1], self._mers[1:]):
             if pair[0].has_bond(pair[1]):
                 pair[0].next_mer = pair[1]
-                pair[1].previous_mer = pair[0]
+                pair[1].prev_mer = pair[0]
 
 
 class AbstractStructure(metaclass=ABCMeta):
@@ -579,7 +579,7 @@ class ElementChainable(AbstractElement, Segment):
             start = self._mers[0]
             end = self._mers[-1]
             try:
-                self._mers = (start.previous_mer,) + self._mers + (end.next_mer,)
+                self._mers = (start.prev_mer,) + self._mers + (end.next_mer,)
             except AttributeError:
                 raise ValueError(msg)
         if self._mers.count(None) != 0:
