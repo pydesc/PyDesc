29429fe1ca1f387c73686bd2c3b65826ca924bd2
vdhert <tymoteusz.oleniecki@gmail.com>
2020-10-13 00:55:21 +0200

    Implement basic alignment methods
    * convertion between jained pas and column
    * join and concatenate
    * limit structures
    * pair alignment equality

--- pydesc/alignment/base.py
+++ pydesc/alignment/base.py
@@ -1,41 +1,170 @@
 """Basic classes storing alignments."""
 
-from abc import ABCMeta
+from abc import ABC
+from abc import abstractmethod
 
 import numpy
 
 DASH = object()
 
 
-class Alignment(metaclass=ABCMeta):
-    pass
+class AbstractAlignment(ABC):
+
+    @abstractmethod
+    def limit_to_structures(self, *structures):
+        pass
 
 
-class AbstractColumnAlignment(Alignment):
+class AbstractJoinedPairAlignments(AbstractAlignment):
+
+    @property
+    @abstractmethod
+    def pair_alignments(self):
+        pass
+
+    @abstractmethod
+    def to_columns(self):
+        pass
+
+    def join(self, other):
+        pair_alignments = set(other.pair_alignments)
+        pair_alignments |= set(self.pair_alignments)
+        return JoinedPairAlignments(tuple(pair_alignments))
+
+
+class AbstractColumnAlignment(AbstractAlignment):
     def __init__(self, structures, inds_rows):
         self.structures = tuple(structures)
         self.inds = inds_rows
 
-    def simplify(self):
+    @abstractmethod
+    def to_joined_pairs(self):
+        pass
+
+    def prune(self):
         _, n_structures = self.inds.shape
         n_nans = numpy.count_nonzero(self.inds == DASH, axis=1)
         self.inds = self.inds[n_nans < (n_structures - 1)]
 
+    def concatenate(self, other):
+        # TODO: adding rows (more aligned mers)
+        other_indices = other.get_structure_indices()
+        column_inds = [other_indices[structure] for structure in self.structures]
+        other_array = other.inds[:, column_inds]
+        all_inds = numpy.concatenate((self.inds, other_array))
+        inds = numpy.unique(all_inds, axis=0)
+        return MultipleColumnsAlignment(self.structures, inds)
 
-class PairAlignment(AbstractColumnAlignment):
+    def get_structure_indices(self):
+        indices = {structure: i for i, structure in enumerate(self.structures)}
+        return indices
+
+
+class PairAlignment(AbstractColumnAlignment, AbstractJoinedPairAlignments):
     def __init__(self, structures, inds_rows):
         if len(structures) != 2:
             raise ValueError("Pair alignment requires exactly two structures.")
         super().__init__(structures, inds_rows)
 
+    def __eq__(self, other):
+        if self is other:
+            return True
+        if set(self.structures) != set(other.structures):
+            return False
+        order = [0, 1]
+        if self.structures != other.structures:
+            order = [1, 0]
+        comparison = self.inds == other.inds[:, order]
+        return numpy.all(comparison)
+
+    def __hash__(self):
+        return hash(self.structures)
+
+    @property
+    def pair_alignments(self):
+        return [self]
+
+    def limit_to_structures(self, *structures):
+        if set(structures) != set(self.structures):
+            msg = "Pair alignment limit can only be trivial." \
+                  "Make sure passed structures are already in it."
+            raise ValueError(msg)
+        return self
+
     def transit(self, alignment):
         pass
 
+    def to_joined_pairs(self):
+        return self
+
+    def to_columns(self):
+        return self
+
 
 class MultipleColumnsAlignment(AbstractColumnAlignment):
-    pass
+
+    def limit_to_structures(self, *structures):
+        if len(structures) < 2:
+            msg = "At least two structure are necessary to perform this operation."
+            raise ValueError(msg)
+        structures_inds = self.get_structure_indices()
+        column_inds = [structures_inds[structure] for structure in structures]
+        inds = self.inds[:, column_inds]
+        if len(column_inds) == 2:
+            return PairAlignment(structures, inds)
+        return MultipleColumnsAlignment(structures, inds)
+
+    def to_joined_pairs(self):
+        structure_indices = self.get_structure_indices()
+        alignments = []
+        for i, stc1 in enumerate(self.structures):
+            for stc2 in self.structures[i + 1:]:
+                structures = stc1, stc2
+                columns_inds = [structure_indices[stc] for stc in structures]
+                array = self.inds[:, columns_inds]
+                alignment = PairAlignment((stc1, stc2), array)
+                alignment.prune()
+                alignments.append(alignment)
+        alignment = JoinedPairAlignments(alignments)
+        return alignment
 
 
-class JoinedPairAlignments(Alignment):
+class JoinedPairAlignments(AbstractJoinedPairAlignments):
     def __init__(self, pair_alignments):
-        self.pair_alignments = pair_alignments
+        self._pair_alignments = pair_alignments
+
+    @property
+    def pair_alignments(self):
+        return self._pair_alignments
+
+    def limit_to_structures(self, *structures):
+        alignments = []
+        for alignment in self.pair_alignments:
+            if set(alignment.structures).issubset(structures):
+                alignments.append(alignment)
+        if len(alignments) == 1:
+            return max(alignments)
+        return JoinedPairAlignments(alignments)
+
+    def to_columns(self):
+        structures = set()
+        length = 0
+        for alignment in self.pair_alignments:
+            structures |= set(alignment.structures)
+            length += alignment.inds.shape[0]
+        structures = tuple(structures)
+
+        structure_indices = {structure: i for i, structure in enumerate(structures)}
+        array = numpy.full((length, len(structures)), DASH)
+
+        start = 0
+        for alignment in self.pair_alignments:
+            columns_indices = [
+                structure_indices[structure] for structure in alignment.structures
+            ]
+            end = start + alignment.inds.shape[0]
+            array[start:end, columns_indices] = alignment.inds
+            start = end
+
+        alignment = MultipleColumnsAlignment(structures, array)
+        return alignment
--- tests/alignments_test.py
+++ tests/alignments_test.py
@@ -4,12 +4,36 @@ from unittest.mock import MagicMock
 import numpy
 import pytest
 
+from pydesc.alignment.base import JoinedPairAlignments
 from pydesc.alignment.base import MultipleColumnsAlignment
 from pydesc.alignment.base import PairAlignment
 from pydesc.alignment.loaders import CSVLoader
+from pydesc.alignment.loaders import DASH
 from pydesc.alignment.loaders import FASTALoader
-from pydesc.alignment.loaders import PALLoader, DASH
+from pydesc.alignment.loaders import PALLoader
 from pydesc.api.structure import get_structures_from_file
+
+
+def get_n_mocked_structures(n, *, start=0):
+    return [MagicMock(name=f"stc{i}") for i in range(start, start + n)]
+
+
+def get_trivial_pair_alignment(stc1, stc2):
+    arr = numpy.array([[i, i] for i in range(10)])
+    return PairAlignment((stc1, stc2), arr)
+
+
+def get_3_pair_alignments():
+    stc1, stc2, stc3 = get_n_mocked_structures(3)
+    pa1 = get_trivial_pair_alignment(stc1, stc2)
+    pa2 = get_trivial_pair_alignment(stc1, stc3)
+    pa3 = get_trivial_pair_alignment(stc2, stc3)
+    return pa1, pa2, pa3
+
+
+@pytest.fixture
+def mocked_structures3():
+    return get_n_mocked_structures(3)
 
 
 class TestLoaders:
@@ -166,13 +190,181 @@ class TestColumnAlignment:
     def test_simplify_pair(self):
         payload = numpy.array([[0, DASH], [1, 0]])
         alignment = PairAlignment([None, None], payload)
-        alignment.simplify()
+        alignment.prune()
 
         assert alignment.inds.shape == (1, 2)
 
     def test_simplify_multi(self):
-        payload = numpy.array([[0, DASH, 0], [1, 0, 1], [DASH, DASH, 4],])
+        payload = numpy.array([[0, DASH, 0], [1, 0, 1], [DASH, DASH, 4], ])
         alignment = MultipleColumnsAlignment([None, None, None], payload)
-        alignment.simplify()
+        alignment.prune()
 
         assert alignment.inds.shape == (2, 3)
+
+    @pytest.fixture
+    def trivial_array3(self):
+        array = numpy.array([
+            [1, 1, 1],
+            [2, 2, 2],
+            [3, 3, 3],
+            [4, 4, 4]
+        ])
+        return array
+
+    def test_columns_to_joined_pairs(self, mocked_structures3):
+        payload = numpy.array([[0, 0, 0], [1, 1, 1], [DASH, 2, 2]])
+        alignment = MultipleColumnsAlignment(mocked_structures3, payload)
+        jp_alignment = alignment.to_joined_pairs()
+
+        assert len(jp_alignment.pair_alignments) == 3
+        shapes = set()
+        for pa in jp_alignment.pair_alignments:
+            shapes.add(pa.inds.shape)
+
+        assert shapes == {(3, 2), (2, 2), (2, 2)}
+
+    def test_concatenate(self, mocked_structures3):
+        # GIVEN
+        arr1 = numpy.array([
+            [1, 1, 1],
+            [2, 2, 3]
+        ])
+        arr2 = numpy.array([
+            [2, 3, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+        ])
+        stc1, stc2, stc3 = mocked_structures3
+        structure_new_order = stc1, stc3, stc2
+        al1 = MultipleColumnsAlignment(mocked_structures3, arr1)
+        al2 = MultipleColumnsAlignment(structure_new_order, arr2)
+
+        # WHEN
+        al3 = al1.concatenate(al2)
+
+        # THEN
+        assert al3.inds.shape == (4, 3)
+
+    def test_limit_structures(self):
+        structures = get_n_mocked_structures(4)
+        array = numpy.array([[i] * 4 for i in range(5)])
+        alignment = MultipleColumnsAlignment(structures, array)
+        stc1, stc2, stc3, stc4 = structures
+
+        # WHEN
+        with pytest.raises(ValueError):
+            alignment.limit_to_structures(stc1)
+        new_pa = alignment.limit_to_structures(stc1, stc2)
+        new_ma = alignment.limit_to_structures(stc2, stc1, stc4)
+
+        # THEN
+        assert new_pa.inds.shape == (5, 2)
+        assert new_ma.inds.shape == (5, 3)
+
+
+class TestJoinedAlignments:
+
+    def test_join(self):
+        pair_alignments1 = get_3_pair_alignments()
+        alignment1 = JoinedPairAlignments(pair_alignments1)
+        different_pair_alignments = get_3_pair_alignments()
+        alignment2 = JoinedPairAlignments(different_pair_alignments)
+
+        single_pa = different_pair_alignments[0]
+        already_included = pair_alignments1[0]
+
+        # WHEN
+        alignment3 = alignment1.join(alignment2)
+        alignment4 = alignment1.join(single_pa)
+        alignment5 = alignment1.join(already_included)
+
+        # THEN
+        assert len(alignment3.pair_alignments) == 6
+        assert len(alignment4.pair_alignments) == 4
+        assert len(alignment5.pair_alignments) == 3
+
+    def test_joined_pair_to_column(self, mocked_structures3):
+        stc1, stc2, stc3 = mocked_structures3
+
+        pair_array1 = numpy.array([[0, 0], [1, 1], [2, 2]])
+        pair_array2 = numpy.array([[0, 0], [1, 1], [3, 3]])
+        pair_al1 = PairAlignment([stc1, stc2], pair_array1)
+        pair_al2 = PairAlignment([stc1, stc3], pair_array2)
+
+        alignment = JoinedPairAlignments((pair_al1, pair_al2))
+
+        column_alignment = alignment.to_columns()
+
+        assert column_alignment.inds.shape == (6, 3)
+        for row in column_alignment.inds:
+            assert DASH in row
+
+    def test_limit_to_2_structures(self):
+        pair_alignments = get_3_pair_alignments()
+        alignment = JoinedPairAlignments(pair_alignments)
+        stc1, stc2 = pair_alignments[0].structures
+
+        limited_pa = alignment.limit_to_structures(stc1, stc2)
+
+        assert isinstance(limited_pa, PairAlignment)
+
+    def test_limit_to_multiple_structures(self):
+        structures = get_n_mocked_structures(4)
+        pas = []
+        for i, stc1 in enumerate(structures):
+            for stc2 in structures[i + 1:]:
+                arr = numpy.array([[l, l] for l in range(6)])
+                pa = PairAlignment((stc1, stc2), arr)
+                pas.append(pa)
+        alignment = JoinedPairAlignments(pas)
+
+        limited = alignment.limit_to_structures(*structures[:-1])
+
+        assert len(limited.pair_alignments) == 3
+
+
+class TestPairAlignment:
+
+    @pytest.fixture
+    def pair_alignment(self):
+        stc1, stc2 = get_n_mocked_structures(2)
+        pa = get_trivial_pair_alignment(stc1, stc2)
+        return pa
+
+    def test_init(self, mocked_structures3):
+        with pytest.raises(ValueError):
+            PairAlignment(mocked_structures3, None)
+        pa = PairAlignment(mocked_structures3[:-1], None)
+
+        assert pa.inds is None
+
+    def test_conversion(self, pair_alignment):
+        same_pa = pair_alignment.to_columns()
+        also_this_pa = pair_alignment.to_joined_pairs()
+
+        assert pair_alignment is same_pa
+        assert pair_alignment is also_this_pa
+
+    def test_limit(self, pair_alignment):
+        stc3, = get_n_mocked_structures(1, start=2)
+        with pytest.raises(ValueError):
+            pair_alignment.limit_to_structures(stc3)
+
+        new_pa = pair_alignment.limit_to_structures(*pair_alignment.structures)
+
+        assert new_pa is pair_alignment
+
+    def test_equality(self, pair_alignment):
+        very_similar_pa = get_trivial_pair_alignment(*pair_alignment.structures)
+
+        assert pair_alignment == very_similar_pa
+
+        stc3, stc4 = get_n_mocked_structures(2, start=2)
+        pa_w_different_structures = get_trivial_pair_alignment(stc3, stc4)
+        assert not pa_w_different_structures == pair_alignment
+        assert pa_w_different_structures != pair_alignment
+
+        pa_different_mers = get_trivial_pair_alignment(*pair_alignment.structures)
+        pa_different_mers.inds = pa_different_mers.inds[:3]
+
+        assert not pa_different_mers == pair_alignment
