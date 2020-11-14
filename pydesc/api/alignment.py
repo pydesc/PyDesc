73b8d42276d7daf0ff10658d219f44b2ee5a9b5a
vdhert <tymoteusz.oleniecki@gmail.com>
2020-11-14 23:10:41 +0100

    Add docstrings to api functions

--- pydesc/api/alignment.py
+++ pydesc/api/alignment.py
@@ -1,8 +1,36 @@
+# Copyright 2020 Tymoteusz Oleniecki
+#
+# This file is part of PyDesc.
+#
+# PyDesc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyDesc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyDesc.  If not, see <http://www.gnu.org/licenses/>.
+"""Functions facilitating usage of alignments."""
+
 from pydesc.alignment.base import DASH
 from pydesc.api.selection import get_selection_from_sub_structure
 
 
 def get_selections(alignment):
+    """Get sets of PDB ids of aligned mers.
+
+    Args:
+        alignment: instance of pydesc.alignments.base.AbstractAlignment subclass
+            representing aligned structures.
+
+    Returns:
+        dict: with structures being keys and selections (Sets) being values.
+
+    """
     dct = get_partial_structures(alignment)
     for structure, sub_structure in dct.items():
         selection = get_selection_from_sub_structure(sub_structure)
@@ -11,6 +39,16 @@ def get_selections(alignment):
 
 
 def get_partial_structures(alignment):
+    """Get partial structures containing only aligned mers.
+
+    Args:
+        alignment: instance of pydesc.alignments.base.AbstractAlignment subclass
+            representing aligned structures.
+
+    Returns:
+        dict: with structures being keys and partial structures being values.
+
+    """
     structures = alignment.structures
     result = {}
     for col, structure in zip(alignment.iter_columns(), structures):
--- pydesc/api/selection.py
+++ pydesc/api/selection.py
@@ -1,8 +1,27 @@
+# Copyright 2020 Tymoteusz Oleniecki
+#
+# This file is part of PyDesc.
+#
+# PyDesc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyDesc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyDesc.  If not, see <http://www.gnu.org/licenses/>.
+"""Functions facilitating usage of selections."""
+
 from pydesc.selection import Set
 
 
-def get_selection_from_sub_structure(sub_structure):
-    converter = sub_structure.derived_from.converter
-    pdb_ids = [converter.get_pdb_id(mer.ind) for mer in sub_structure]
+def get_selection_from_sub_structure(substructure):
+    """Return selection of PDB ids of mers from given substructure."""
+    converter = substructure.derived_from.converter
+    pdb_ids = [converter.get_pdb_id(mer.ind) for mer in substructure]
     selection = Set(pdb_ids)
     return selection
