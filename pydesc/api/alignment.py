0244320d7cf939778e10494bc174e9642882890e
vdhert <tymoteusz.oleniecki@gmail.com>
2020-11-14 19:45:26 +0100

    Add api piciking substructres from alignments

--- pydesc/api/alignment.py
+++ pydesc/api/alignment.py
@@ -1,13 +1,20 @@
-from pydesc.selection import Set
 from pydesc.alignment.base import DASH
+from pydesc.api.selection import get_selection_from_sub_structure
 
 
 def get_selections(alignment):
-    selections = {}
-    for col, structure in enumerate(alignment.structures):
-        converter = structure.converter
-        inds = alignment.inds[:, col].flatten()
-        pdb_ids = [converter.get_pdb_id(ind) for ind in inds if ind is not DASH]
-        selection = Set(pdb_ids)
-        selections[structure] = selection
-    return selections
+    dct = get_partial_structures(alignment)
+    for structure, sub_structure in dct.items():
+        selection = get_selection_from_sub_structure(sub_structure)
+        dct[structure] = selection
+    return dct
+
+
+def get_partial_structures(alignment):
+    structures = alignment.structures
+    result = {}
+    for col, structure in zip(alignment.iter_columns(), structures):
+        inds = col[col != DASH]
+        partial_structure = structure[inds]
+        result[structure] = partial_structure
+    return result
--- tests/alignment_tests/api_test.py
+++ tests/alignment_tests/api_test.py
@@ -1,8 +1,28 @@
 import numpy
+import pytest
+
 from pydesc.alignment.base import DASH
-from pydesc.api.alignment import get_selections
 from pydesc.alignment.loaders import PALLoader
+from pydesc.api.alignment import get_partial_structures
+from pydesc.api.alignment import get_selections
 from pydesc.api.structure import get_structures_from_file
+
+
+@pytest.fixture(scope="session")
+def kinases(alignments_dir):
+    stc_path = alignments_dir / "structures" / "kinases3_dama.pdb"
+    structures = get_structures_from_file(stc_path)
+    return structures
+
+
+@pytest.fixture(scope="session")
+def kinases_alignment(alignments_dir, kinases):
+    path = alignments_dir / "pal" / "kinases3_dama.pal"
+    loader = PALLoader(path)
+
+    alignment = loader.load_alignment(kinases)
+    alignment = alignment.close()
+    return alignment
 
 
 def test_selections_sars(alignments_dir):
@@ -21,18 +41,29 @@ def test_selections_sars(alignments_dir):
         numpy.testing.assert_array_equal(expected_inds, inds)
 
 
-def test_selections_3_kinases(alignments_dir):
-    stc_path = alignments_dir / "structures" / "kinases3_dama.pdb"
-    structures = get_structures_from_file(stc_path)
-    path = alignments_dir / "pal" / "kinases3_dama.pal"
-    loader = PALLoader(path)
-
-    alignment = loader.load_alignment(structures)
-
-    selections_dct = get_selections(alignment)
-
-    for structure, expected_inds in zip(alignment.structures, alignment.iter_columns()):
+def test_selections_3_kinases(kinases_alignment):
+    selections_dct = get_selections(kinases_alignment)
+    for structure, expected_inds in zip(
+        kinases_alignment.structures, kinases_alignment.iter_columns()
+    ):
         selection = selections_dct[structure]
         inds = selection.get_list_of_inds(structure)
         expected_inds = expected_inds[numpy.where(expected_inds != DASH)]
         numpy.testing.assert_array_equal(expected_inds, inds)
+
+
+def test_get_partial_structures(kinases_alignment):
+    dct = get_partial_structures(kinases_alignment)
+    col_inds_map = kinases_alignment.get_structure_indices()
+
+    for structure, sub_structure in dct.items():
+        assert structure is sub_structure.derived_from
+        col_ind = col_inds_map[structure]
+        col = kinases_alignment.inds[:, col_ind]
+        col_len = numpy.count_nonzero(col[col != DASH])
+        assert len(sub_structure) == col_len
+        assert len(sub_structure) > 46
+
+    # order of columns is different
+    lens = {len(item) for item in dct.values()}
+    assert lens == {238, 47}
