80a26aed0c0be3b6a8eff4eb5dca782f91c82ded
vdhert <tymoteusz.oleniecki@gmail.com>
2021-11-07 19:18:50 +0100

    Fix alignment factory test

--- tests/alignment_tests/factories_test.py
+++ tests/alignment_tests/factories_test.py
@@ -16,5 +16,5 @@ def test_from_list_of_inds(structure):
     structures = [structure] * 3
     inds_lists = ((0, 1, 2, 4), (2, 3, 4, 5), (1, None, 42, 24))
     alignment = factory.create_from_list_of_inds(structures, inds_lists)
-    assert alignment.inds.shape == (3, 4)
-    assert alignment.inds[1, 3] is DASH
+    assert alignment.inds.shape == (4, 3)
+    assert alignment.inds[1, 2] is DASH
